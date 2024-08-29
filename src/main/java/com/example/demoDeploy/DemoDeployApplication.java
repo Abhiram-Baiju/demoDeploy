2d0362afde93464dc70a7e0a02cf10f196d7a15d
Abhiram <abhiram@seemymachines.com>
2024-08-29 14:17:56 +0530

    demoDeploy

---
+++ src/main/java/com/example/demoDeploy/DemoDeployApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoDeployApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployApplication.class, args);
+	}
+
+}
